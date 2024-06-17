b8060230567bc982a9c91141e103abe755e740f2
[IoT3] 이다은 <lgc6466@naver.com>
2024-06-17 11:46:35 +0900

    배송지 수정 모달창

--- src/components/orders/OrderItem.jsx
+++ src/components/orders/OrderItem.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import Modal from "react-modal";
 import {
   OrderDate,
   OrderStatus,
@@ -17,12 +18,37 @@ import {
   OrderActions,
 } from "./styles/PageStyles";
 
+import OrderEdit from "./OrderEdit";
+
+const customStyles = {
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)',
+    width: '500px',
+    height: '370px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'transparent',
+    border: 'none'
+  },
+};
+
 const OrderItem = ({ order }) => {
   const { order_date, status, product, order_id } = order;
   const { name: productName, variants } = product;
   const { price, images, color, size, stock } = variants[0];
 
   const totalPrice = price * stock;
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => setIsModalOpen(true);
+  const closeModal = () => setIsModalOpen(false);
 
   return (
     <OrderContainer>
@@ -34,9 +60,9 @@ const OrderItem = ({ order }) => {
             <ProductImage src={images[0]} alt={productName} />
             <div>
               <OrderProduct>{`${productName} - ${color} / ${size}`}</OrderProduct>
-              <div style={{display : "flex"}}>
-              <OrderPrice>{`${price.toLocaleString("ko-KR")} 원 -`}</OrderPrice>
-              <OrderPrice>{`총 금액 : ${totalPrice.toLocaleString("ko-KR")} 원`}</OrderPrice>
+              <div style={{ display: "flex" }}>
+                <OrderPrice>{`${price.toLocaleString("ko-KR")} 원 -`}</OrderPrice>
+                <OrderPrice>{`총 금액 : ${totalPrice.toLocaleString("ko-KR")} 원`}</OrderPrice>
               </div>
             </div>
           </ProductDetails>
@@ -44,13 +70,23 @@ const OrderItem = ({ order }) => {
         <OrderRightSection>
           <OrderActions>
             <OrderPrice>{`ID - #${order_id}`}</OrderPrice>
-            <div style={{display : "flex", gap : "5px"}}>
-            <OrderActionsButton>주문 취소</OrderActionsButton>
-            <OrderActionsButton>주문 수정</OrderActionsButton>
+            <div style={{ display: "flex", gap: "5px" }}>
+              <OrderActionsButton>주문 취소</OrderActionsButton>
+              <OrderActionsButton onClick={openModal}>주문 수정</OrderActionsButton>
             </div>
           </OrderActions>
         </OrderRightSection>
       </OrderContent>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Order Edit Modal"
+        ariaHideApp={false}
+        style={customStyles}
+      >
+        <OrderEdit order={order} closeModal={closeModal} />
+      </Modal>
     </OrderContainer>
   );
 };
