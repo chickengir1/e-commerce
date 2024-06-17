acc2773157f010f1926fd6207ae8efa8b697fda4
Lee gangho <rkdgh1190@gmail.com>
2024-06-17 19:40:10 +0900

    핫픽스 - 비밀번호 변경시 알림 창 사라지지 않는 버그

--- src/hook/usePasswordChange.jsx
+++ src/hook/usePasswordChange.jsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import API_PATHS from "../utils/apiPaths";
 
 const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
@@ -8,26 +7,31 @@ const usePasswordChange = (apiUrl) => {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [notification, setNotification] = useState("");
 
-  const validatePasswords = () => {
+  const delayNotification = async (message) => {
+    setNotification(message);
+    await delay(1500);
+    setNotification("");
+  };
+
+  const validatePasswords = async () => {
     if (newPassword === "" || confirmPassword === "") {
-      setNotification("모든 필드를 입력해주세요.");
+      await delayNotification("모든 필드를 입력해주세요.");
       return false;
     }
     if (newPassword !== confirmPassword) {
-      setNotification("비밀번호가 일치하지 않습니다.");
+      await delayNotification("비밀번호가 일치하지 않습니다.");
       return false;
     }
     if (newPassword.length < 8) {
-      setNotification("비밀번호는 최소 8자리여야 합니다.");
+      await delayNotification("비밀번호는 최소 8자리여야 합니다.");
       return false;
     }
-    setNotification("");
     return true;
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if (validatePasswords()) {
+    if (await validatePasswords()) {
       try {
         const response = await fetch(apiUrl, { 
           method: "PUT",
@@ -41,13 +45,9 @@ const usePasswordChange = (apiUrl) => {
           throw new Error("Failed to update password");
         }
 
-        setNotification("비밀번호가 성공적으로 변경되었습니다.");
-        await delay(1500);
-        setNotification("");
+        await delayNotification("비밀번호가 성공적으로 변경되었습니다.");
       } catch (error) {
-        setNotification("비밀번호 변경에 실패했습니다.");
-        await delay(1500);
-        setNotification("");
+        await delayNotification("비밀번호 변경에 실패했습니다.");
       }
     }
   };
